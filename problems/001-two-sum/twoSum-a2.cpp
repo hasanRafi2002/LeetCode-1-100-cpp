0f30692cee4c8357a9ad5c6d4f79a2ef2cfb7b15
hasanRafi2002 <rafi2002ten@gmail.com>
2025-05-24 15:51:19 +0600

    rafi

---
+++ problems/001-two-sum/twoSum-a2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int n, target;
+
+    cout<<"Enter number of elements: ";
+    cin>>n;
+
+    vector<int> nums(n);
+    cout<<"Enter the elements:\n";
+    for(int i = 0; i<n; ++i){
+        cin>>nums[i];
+    }
+    cout<<"Enter target: ";
+    cin>>target;
+
+
+for(int i=0; i<n; ++i){
+    for(int j=i+1; j<n; ++j){
+        if(nums[i]+ nums[j] == target){
+            cout<<"Indices: ["<<i<<", "<<j<<"]\n";
+            return 0;
+        }
+    }
+}
+
+cout<<"No valid pair found.\n";
+return 0;
+
+
+
+
+
+}
