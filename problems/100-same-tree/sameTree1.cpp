60a818117ad584b633217a3c65dc71f4e37cff42
hasanRafi2002 <rafi2002ten@gmail.com>
2025-06-09 23:39:24 +0600

    rafi

---
+++ problems/100-same-tree/sameTree1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr){}
+
+
+};
+
+TreeNode* buildTree(){
+    cout<<"Enter tree nodes in level order (use -1 for NULL): ";
+
+    int val;
+    cin>>val;
+
+    if(val == -1) return nullptr;
+}
